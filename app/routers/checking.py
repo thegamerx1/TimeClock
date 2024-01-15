763fba79f32ee1968c8c411c7b88f0c15be1875c
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2024-01-15 10:22:40 +0100

    feat: fichar en db

--- app/routers/checking.py
+++ app/routers/checking.py
@@ -7,6 +7,7 @@ from app.utils.database import get_db
 from app.utils.tts import generate_tts, tts_from_id
 
 from typing import Annotated
+from datetime import datetime
 
 router = APIRouter()
 
@@ -32,33 +33,33 @@ async def get_login(request: Request):
     return templates.TemplateResponse("pages/login.html", context)
 
 
-@router.post(path="/login", summary="Logs into the app", tags=["Authentication"])
-async def post_login(
-    userId: Annotated[str, Form()], pin: Annotated[str, Form()]
-) -> dict:
-    with get_db() as db:
-        db.cursor.execute(f"SELECT * FROM Personas WHERE Id = (?)", (userId))
-        persona = db.cursor.fetchone()
+# @router.post(path="/login", summary="Logs into the app", tags=["Authentication"])
+# async def post_login(
+#     userId: Annotated[str, Form()], pin: Annotated[str, Form()]
+# ) -> dict:
+#     with get_db() as db:
+#         db.cursor.execute(f"SELECT * FROM Personas WHERE Id = (?)", (userId))
+#         persona = db.cursor.fetchone()
 
-    if persona is None or str(persona[3]) != pin:
-        print("bad :(")
-        return {"success": False}
+#     if persona is None or str(persona[3]) != pin:
+#         print("bad :(")
+#         return {"success": False}
 
-    # TODO: Fichar en la DB
+#     # TODO: Fichar en la DB
 
-    return {"success": True}
+#     return {"success": True}
 
 
 @router.post(
     path="/loginCodigoQR", summary="Logs into the app", tags=["Authentication"]
 )
 async def post_login(pinCodigoQR: Annotated[str, Form()]) -> dict:
-    with get_db() as db:
-        db.cursor.execute(
-            f"SELECT * FROM Personas WHERE PINCodigoQR = (?)",
-            (pinCodigoQR),
-        )
-        persona = db.cursor.fetchall()
+    db = get_db()
+    db.cursor.execute(
+        f"SELECT * FROM Personas WHERE PINCodigoQR = (?)",
+        (pinCodigoQR),
+    )
+    persona = db.cursor.fetchall()
 
     if persona is None:
         return {"success": False}
@@ -66,13 +67,34 @@ async def post_login(pinCodigoQR: Annotated[str, Form()]) -> dict:
     persona = persona[0]
     print(persona)
 
+    fichajes = db.cursor.execute(
+        f"SELECT * FROM Fichajes WHERE IdPersona = {persona[0]} AND FechaSalida Is Null ORDER BY FechaEntrada"
+    )
+
+    laprimera = fichajes.fetchone()
+    now = datetime.now()
+
+    if not laprimera == None and laprimera[3] == None:
+        sql = f"UPDATE Fichajes SET FechaSalida = ? WHERE Id = {laprimera[0]}"
+        mensaje = "Que tengas un buen día"
+    else:
+        sql = f"INSERT INTO Fichajes (FechaEntrada,IdPersona,Metodo) VALUES (?,{persona[0]},1)"
+        mensaje = "Hola, bienvenido"
+
+    db.cursor.execute(sql, (now))
+
     nombre = f"{persona[2]}, {persona[1]}"
+    nombre_correcto = f"{persona[1]} {persona[2]}"
 
-    id_voz = generate_tts(f"Hola, bienvenido {nombre}")
+    id_voz = generate_tts(f"{mensaje} {nombre_correcto}")
 
-    # TODO: Fichar en la DB
-
-    return {"success": True, "username": nombre, "voz_id": id_voz}
+    db.commit()
+    db.close()
+    return {
+        "success": True,
+        "mensaje": f"{mensaje} {nombre}",
+        "voz_id": id_voz,
+    }
 
 
 @router.get(path="/voz/{id_voz}", summary="Da el archivo de voz")
--- app/utils/database.py
+++ app/utils/database.py
@@ -4,14 +4,19 @@ from app import db_path
 
 class get_db:
     def __init__(self):
-        return None
-
-    def __enter__(self):
         self.conn = pyodbc.connect(
             r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=" + db_path + ";"
         )
         self.cursor = self.conn.cursor()
+
+    def __enter__(self):
         return self
+
+    def close(self):
+        self.__exit__(None, None, None)
+
+    def commit(self):
+        self.conn.commit()
 
     def __exit__(self, _a, _b, _c):
         self.cursor.close()
