a1c1f84709fc9600ba7664ffa2b7ee78f9ebdf19
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2024-01-19 08:37:54 +0100

    feat: Fix script for starting chrome

--- run.ps1
+++ run.ps1
@@ -1,4 +1,17 @@
 cd $PSScriptRoot
 & .\venv\Scripts\Activate.ps1
+Start-Job -ScriptBlock {
+	$WEBPAGE = "http://localhost:8000"
+	While($true) {
+		Start-Sleep -Seconds 2
+		try {
+			Invoke-WebRequest -Uri $WEBPAGE
+			break
+		} catch {
+			continue
+		}
+	}
+	& "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk $WEBPAGE
+}
 uvicorn app.main:app
 deactivate
