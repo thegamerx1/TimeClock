3e115d7dac2251cf2a025d4ea02f2166ba679a76
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2024-01-25 10:07:53 +0100

    feat: Add window activation

--- run.ps1
+++ run.ps1
@@ -1,17 +1,42 @@
 cd $PSScriptRoot
 & .\venv\Scripts\Activate.ps1
+Add-Type @"
+	using System;
+	using System.Runtime.InteropServices;
+	public class WinAp {
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+	}
+"@
+
 Start-Job -ScriptBlock {
 	$WEBPAGE = "http://localhost:8000"
-	While($true) {
+	While ($true) {
 		Start-Sleep -Seconds 2
 		try {
 			Invoke-WebRequest -Uri $WEBPAGE
 			break
-		} catch {
+		}
+		catch {
 			continue
 		}
 	}
-	& "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk $WEBPAGE
+	Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--kiosk", "$WEBPAGE"
+
+	While ($true) {
+		Start-Sleep -Seconds 5
+		$p = Get-Process | Where-Object { $_.mainWindowTitle -and $_.Name -like "chrome" }
+		if ($p) {
+			$handle = $p.MainWindowHandle
+			[void] [WinAp]::SetForegroundWindow($handle)
+			[void] [WinAp]::ShowWindow($handle, 3)
+		}
+	}
 }
 uvicorn app.main:app
 deactivate
